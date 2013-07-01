d42f76b3e29638cb2ed3a580443fc3ccbd1ad7f9
daniel-navia <daniel.navia@live.com>
2013-07-01 14:43:56 -0700

    addins test no life can exists of the edges

--- spec/gameOfLifeSpec.js
+++ spec/gameOfLifeSpec.js
@@ -1,8 +1,14 @@
-describe("Game Of Life", function() {
+describe("Game Of Life Kata", function() {
 
 	it("need a two dimensional grid", function() {
 	  
-		var result = new Board();
+		var result = new Board(1,2);
 		expect(result).toBeDefined();
+	});
+
+	it("in the edges of board no life can exists", function() {
+	  	
+	  	var _board = new Board(2, 2);
+	  	expect(_board.cellAlive()).toBeEqual(0);
 	});
 });
