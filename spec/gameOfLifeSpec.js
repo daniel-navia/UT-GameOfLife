a36225054669e50abf6a3c88c6e95a7d5d2558e5
daniel-navia <daniel.navia@live.com>
2013-07-01 14:30:24 -0700

    fixing toBedefined() method

--- spec/gameOfLifeSpec.js
+++ spec/gameOfLifeSpec.js
@@ -3,6 +3,6 @@ describe("Game Of Life", function() {
 	it("need a two dimensional grid", function() {
 	  
 		var result = new Board();
-		expect(result).isDefined();
+		expect(result).toBeDefined();
 	});
 });
