0cfb83165fd94ee3db32f756001389f2826ffe35
daniel-navia <Daniel Navia@B206-01.fundacion-jala.local>
2013-07-02 10:40:43 -0400

    adding spec: any cell alive with fewer than two neighbors die

--- spec/gameOfLifeSpec.js
+++ spec/gameOfLifeSpec.js
@@ -11,4 +11,10 @@ describe("Game Of Life Kata", function() {
 	  	var _board = new Board(2, 2);
 	  	expect(_board.cellAliveInTheEdge()).toEqual(0);
 	});
+
+	it("any live cell with fewer than two neighbors dies.", function() {
+	  
+	  var _board = new Board(4,4);
+	  expect(_board.cellsAlive()).toEqual(0);
+	});
 });
